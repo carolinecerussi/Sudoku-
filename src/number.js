00ccb7d77a4917a13a461f4ed2e3a6162b29bb99
Caroline Cerussi <caroceru@gmail.com>
2023-02-02 20:02:23 -0800

    get number.js to return correct amounts for both texts in console log

--- src/number.js
+++ src/number.js
@@ -1,20 +1,18 @@
+const { number } = require("yargs");
 
-
-export function Number() {
-const numArray = [1,2,3,4,5,6,7,8,9];
-new number = Number(numArray)
-return numArray;
+function Number() {
+  const numArray = [1,2,3,4,5,6,7,8,9];
+  console.log(numArray);  
+  const min = Math.ceil(numArray[0]);
+  const max = Math.ceil(numArray[8]);
+  console.log(min);
+  console.log(max);
+   const random = (Math.floor(Math.random() * (max - min +1) + min)); 
+  console.log(random);
 }
-//first test
+Number();
 
-export function Number(){
-    const numArray = [1,2,3,4,5,6,7,8,9];
-    const min = Math.ceil(numArray[0]);
-    const max = Math.floor(numArray[8]);
-   const randomNum = Math.floor(Math.random() * (max -min +1) + min);
-const number = Number(randomNum);
-expect(number);
-}
+
 
 //second
 
