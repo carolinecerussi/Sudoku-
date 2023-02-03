ed84741a45fbba9cc81062db199382be5d77fb77
Caroline Cerussi <caroceru@gmail.com>
2023-02-02 16:49:33 -0800

    install webpacks, dependancies, and start some css formatting

---
+++ src/number.js
@@ -0,0 +1,6 @@
+export function Number(numArray) {
+  const numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let numEntered = document.querySelector.getElementById("entry").value;
+  console.log(numEntered);
+
+}
